2bb0ca36d4e0665b0133301ca6980418799e9bdc
Donald Ng <donaldyann@gmail.com>
2017-10-01 10:10:01 +0800

    Fixed bug - tracker feeds is not complete.

--- tracker/track_pair.py
+++ tracker/track_pair.py
@@ -15,6 +15,8 @@ ws = create_connection('wss://api.bitfinex.com/ws/')
 sub = json.dumps({'event': 'subscribe', 'channel': 'trades', 'pair':pair})
 ws.send(sub)
 
+count = 0
+
 while True:
     x = json.loads(ws.recv())
 
@@ -23,15 +25,30 @@ while True:
             os.kill(pid, 0)
         except:
             sys.exit()
+    
+    if count < 2:
+        count+=1
+    elif count == 2:
+        count = 99
+        for y in x[1]:
+            # Current orderbooks value
+            ts = y[1]
+            price = y[2]
+            amount = y[3]
+            base = 1 if pair == "BTCUSD" or pair == "ETHUSD" else 0
+
+            data = {'ts':ts, 'price': price,'amount':amount, 'pair': pair, 'base': base}
+            res = db.trades.insert_one(data)
+
 
     if len(x) == 6:
+        # Realtime trades update
         ts = x[3]
         price = x[4]
         amount = x[5]
         base = 1 if pair == "BTCUSD" or pair == "ETHUSD" else 0
 
-        if amount % 10:
-            data = {'ts':ts, 'price': price,'amount':amount, 'pair': pair, 'base': base}
-            res = db.trades.insert_one(data)
+        data = {'ts':ts, 'price': price,'amount':amount, 'pair': pair, 'base': base}
+        res = db.trades.insert_one(data)
 
 ws.close()
